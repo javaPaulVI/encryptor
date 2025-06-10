f4b94afbd39a773a3c89753f2db9a254ace50ca8
Paul Hipper <paul@be-hip.eu>
2025-06-10 22:50:28 +0200

    0.1.1alpha

--- commands/windows/zip_project.bat
+++ commands/windows/zip_project.bat
@@ -1,18 +1,25 @@
 @echo off
-REM Get the full path to this script's directory
+REM Get the path to the folder containing this script
 set "SCRIPT_DIR=%~dp0"
 
-REM Go up three levels to get to the 'encryption' folder
+REM Go up three levels to get to the encryption folder
 pushd "%SCRIPT_DIR%..\..\.."
 set "ENCRYPTION_DIR=%cd%"
 set "ZIPFILE=%ENCRYPTION_DIR%\encryption.zip"
 
-REM Delete old zip if it already exists
+REM Delete old zip if it exists (prevent recursive zipping)
 if exist "%ZIPFILE%" del "%ZIPFILE%"
 
-REM Use PowerShell to zip everything in the folder, excluding the .zip file
-powershell -Command "Get-ChildItem -Path '%ENCRYPTION_DIR%' -Recurse -File | Where-Object { $_.FullName -ne '%ZIPFILE%' } | Compress-Archive -DestinationPath '%ZIPFILE%' -Force"
+REM Zip the contents of the encryption folder (but not the zip file itself)
+powershell -Command ^
+    "$exclude = '%ZIPFILE%';" ^
+    "Add-Type -AssemblyName 'System.IO.Compression.FileSystem';" ^
+    "$zip = [System.IO.Compression.ZipFile]::Open('$ZIPFILE', 'Create');" ^
+    "Get-ChildItem -Path '.' -Recurse | Where-Object { -not $_.PSIsContainer -and $_.FullName -ne $exclude } | ForEach-Object {" ^
+    "  $entryName = $_.FullName.Substring($env:ENCRYPTION_DIR.Length + 1);" ^
+    "  [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $_.FullName, $entryName);" ^
+    "}; $zip.Dispose()"
 
-echo Created ZIP at: %ZIPFILE%
+echo Created ZIP inside: %ZIPFILE%
 pause
 popd
